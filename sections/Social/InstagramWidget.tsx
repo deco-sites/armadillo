e96918c0f69f442919de2ea62b1068d00feb15f0
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-03 18:52:05 -0300

    style: Instagram Widget improved

--- sections/Social/InstagramWidget.tsx
+++ sections/Social/InstagramWidget.tsx
@@ -6,13 +6,14 @@ export default function InstagramWidget({ app }: Props) {
   if (!app) return null;
 
   return (
-    <>
+    <div class="max-w-[50%] mx-auto pt-8 pb-12">
       <script
         src="https://static.elfsight.com/platform/platform.js"
         data-use-service-core
         defer
       />
+
       <div class={app} data-elfsight-app-lazy />
-    </>
+    </div>
   );
 }
