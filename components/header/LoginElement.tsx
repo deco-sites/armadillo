3c4d3009964a305c74cace63fa6ddb3ba860c763
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-06 17:56:20 -0300

    style: fonts adjusted

--- components/header/LoginElement.tsx
+++ components/header/LoginElement.tsx
@@ -4,7 +4,7 @@ export default function LoginElement() {
       <label tabIndex={0} class="font-bold uppercase text-xs">Login</label>
       <ul
         tabIndex={0}
-        class="dropdown-content translate-x-[25%] z-[1] shadow bg-base-100 rounded-none w-48 text-black font-semibold pb-2 pt-5 uppercase text-sm"
+        class="dropdown-content translate-x-[25%] z-[1] shadow bg-base-100 rounded-none w-48 text-black font-semibold pb-2 pt-5 uppercase text-[13px] tracking-[1px]"
       >
         <li class="flex hover:bg-[#3cb14e] py-0.5 pl-2 my-3 w-full">
           <a href="/account" class="flex w-full">Minha Conta</a>
