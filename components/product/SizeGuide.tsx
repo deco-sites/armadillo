494a39c52b622604fbc9f217049aa37ba13e2392
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-16 00:12:41 -0300

    fix: SizeGuide improved

--- components/product/SizeGuide.tsx
+++ components/product/SizeGuide.tsx
@@ -1,11 +1,3 @@
 export default function SizeGuide() {
-  return (
-    <div
-      id="sizebay-container"
-      class="vfr__container"
-      szb-product-path="p"
-      sizebay-product-reference="43718429"
-    >
-    </div>
-  );
+  return <div id="sizebay-container"></div>;
 }
