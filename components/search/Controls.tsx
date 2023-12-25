12d7b8a944b3b0306ac7d06bb99c7bef07cb30e4
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-25 17:13:34 -0300

    fix (PLP): solving bug in mobile components

--- components/search/Controls.tsx
+++ components/search/Controls.tsx
@@ -20,49 +20,72 @@ function SearchControls(
     window.location.href = currentUrl.toString();
   };
 
+  function handleClick() {
+    const toggleButton = document.getElementById("toggle-view-more")!;
+    const viewMore = document.getElementById("view-more")!;
+
+    toggleButton.addEventListener("click", () => {
+      const isOpen = viewMore.getAttribute("open") === "true";
+
+      if (isOpen) {
+        viewMore.classList.add("max-h-[32px]", "overflow-hidden");
+      } else {
+        viewMore.classList.remove("max-h-[32px]", "overflow-hidden");
+      }
+
+      viewMore.setAttribute("open", String(!isOpen));
+    });
+  }
+
   return (
-    <div class="flex flex-col justify-between mb-4 py-4 sm:p-0 lg:max-w-[95%] mx-auto">
-      <div class="flex flex-row items-center mb-2">
-        <Breadcrumb itemListElement={breadcrumb?.itemListElement} />
-      </div>
+    <>
+      <div class="flex flex-col justify-between mb-4 py-4 sm:p-0 lg:max-w-[95%] mx-auto">
+        <div class="flex flex-row items-center mb-2">
+          <Breadcrumb itemListElement={breadcrumb?.itemListElement} />
+        </div>
 
-      <div class="flex flex-col md:flex-row">
-        <div class="collapse w-full cursor-default focus:outline-none rounded-none border-none">
-          <input
-            type="checkbox"
-            class="peer w-[178px] h-[32px] absolute !p-0 rounded-none border-none"
-          />
+        <div
+          id="view-more"
+          class="flex flex-col md:flex-row max-h-[32px] overflow-hidden"
+        >
+          <div class="w-full cursor-default focus:outline-none rounded-none border-none">
+            <div class="flex gap-2 w-full">
+              <div class="w-[195px] h-[32px]">
+                <button
+                  id="toggle-view-more"
+                  aria-label="open filters"
+                  class={displayFilter
+                    ? "flex text-[#666] text-sm uppercase justify-center items-center gap-1 py-0.5 w-[170px] h-[32px] leading-[19px] border border-gray-200"
+                    : "flex justify-center items-center gap-0.5 py-0.5 w-[168px] h-[32px] leading-[19px] border border-gray-200 sm:hidden"}
+                >
+                  Filtros
+                  <Icon id="Filter" width={16} height={16} />
+                </button>
+              </div>
 
-          <div class="flex gap-2 w-full">
-            <div class="collapse-title w-[195px] h-[32px] !p-0">
               <button
-                aria-label="open filters"
-                class={displayFilter
-                  ? "flex text-[#666] text-sm uppercase justify-center items-center gap-1 py-0.5 w-[170px] h-[32px] leading-[19px] border border-gray-200"
-                  : "flex justify-center items-center gap-0.5 py-0.5 w-[168px] h-[32px] leading-[19px] border border-gray-200 sm:hidden"}
+                onClick={removeSort}
+                aria-label="remove filters"
+                class="flex text-[#666] text-sm lowercase justify-center items-center py-0.5 w-[120px] h-[32px] leading-[19px] border border-gray-200"
               >
-                Filtros
-                <Icon id="Filter" width={16} height={16} />
+                Remover filtros
               </button>
             </div>
 
-            <button
-              onClick={removeSort}
-              aria-label="remove filters"
-              class="flex text-[#666] text-sm lowercase justify-center items-center py-0.5 w-[120px] h-[32px] leading-[19px] border border-gray-200"
-            >
-              Remover filtros
-            </button>
-          </div>
-
-          <div class="collapse-content p-0">
-            <div class="flex-grow overflow-auto">
-              <Filters filters={filters} sortOptions={sortOptions} />
+            <div class="p-0">
+              <div class="flex-grow overflow-auto">
+                <Filters filters={filters} sortOptions={sortOptions} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <script
+        defer
+        dangerouslySetInnerHTML={{ __html: `(${handleClick.toString()})()` }}
+      />
+    </>
   );
 }
 
